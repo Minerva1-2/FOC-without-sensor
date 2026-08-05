92db467896163ec470e14269696c491bb134a9f7
minerva <18993035310@163.com>
2026-08-05 22:10:31 +0800

    1.添加鉴相器功能
    2.添加指示灯代码
    
    3.重映射printf到串口一
    
    电流采样修改至sample.c文件中

--- motorControl/hardware/Inc/driver.h
+++ motorControl/hardware/Inc/driver.h
@@ -2,8 +2,11 @@
 #define __DRIVER_H
 
 #include "gpio.h"
+#include "globalControl.h"
 
 void MotorDriverEnable(void);
 void MotorDriverDisable(void);
+void LedON(Motor_t *motor);
+void LedOFF(void);
 
 #endif
